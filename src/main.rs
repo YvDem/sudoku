3c160dfedb2f2e1a042844a2d0635284a447d16c
Yvan <yampleman1401@gmail.com>
2023-06-16 00:00:16 +0200

    added wip branch, merged incomplete code

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,18 @@ use sudoku::Board;
 use sudoku::Player;
 
 fn main() {
-    let mut board = Board {current_player: Player::X, board_content: [0; 9]};
+    let mut board = Board {
+        current_player: Player::X,
+        board_content: [0; 9],
+    };
 
     loop {
-        
         let current_player = board.current_player_symbol();
         println!("C'est au tour de {} de jouer!", current_player);
         board.show_board_content();
-        
-    
-        let player_position = board.ask_position();    
-        board.update_board_position(player_position);
 
+        let player_position = board.ask_position();
+        board.update_board_position(player_position);
 
         let end = board.evaluate_end();
         if end {
