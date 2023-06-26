8e15ac240cebf1ffdc7a2339c41f12e297aafab2
Yv Dem <YvDem.Prog@gmail.com>
2023-06-26 20:29:03 +0200

    still not it

--- src/bin/trees_test.rs
+++ src/bin/trees_test.rs
@@ -4,34 +4,9 @@ use sudoku::{Board, Game, Player};
 use trees::{Init, Node};
 
 fn main() {
-    let mut b_tree = Node::init(Game {
-        current_player: Player::X,
-        board_content: Board {
-            content: [
-                Player::X,
-                Player::X,
-                Player::Empty,
-                Player::O,
-                Player::O,
-                Player::Empty,
-                Player::Empty,
-                Player::Empty,
-                Player::Empty,
-            ],
-        },
-        winner: Player::Empty,
-        closed: false,
-    });
-
+    let mut b_tree = Node::init(Game::default());
     create_all_possibilites_r(&mut b_tree);
-    println!("size: {}", b_tree.size(-1));
-    print!("minimax: {}\n", minimax_r(&mut b_tree, 9, Player::X));
-
-    for snode in b_tree.mut_snodes() {
-        println!("minimax of snode :{}", minimax_r(snode, 9, Player::X))
-    }
-
-    show_possibilities(&b_tree);
+    println!("minimax: {}", minimax(&b_tree, 9, Player::X, true));
 }
 
 fn create_all_possibilites_r(node: &mut Node<Game>) {
@@ -43,37 +18,36 @@ fn create_all_possibilites_r(node: &mut Node<Game>) {
         let mut sboard = node.value().clone();
         sboard.update_board(pos);
 
-        let mut snode = trees::Node::init(sboard);
+        let mut snode = Node::init(sboard);
         create_all_possibilites_r(&mut snode);
         node.add_snode(snode);
     }
 }
 
-fn minimax_r(node: &mut Node<Game>, depth: i32, max_player: Player) -> i32 {
+fn minimax(node: &Node<Game>, depth: i32, player: Player, maxplayer: bool) -> i32 {
     if node.value().closed() || depth == 0 {
-        let value = match node.value().winner() {
-            Player::Empty => 0,
-            e if e == max_player => 1,
-            _ => -1,
-        };
-        return value;
+        match node.value().winner() {
+            Player::Empty => return 0,
+            pl if pl == player => return 10 - depth,
+            _ => return depth - 10,
+        }
     }
 
-    let mut value = -9000;
-    if node.value().c_player() == max_player {
-        for snode in node.mut_snodes() {
-            let b_value = minimax_r(snode, depth - 1, max_player);
-            value = if b_value > value { b_value } else { value }
+    if maxplayer {
+        let mut val = -9000;
+        for snode in node.snodes() {
+            let sval = minimax(snode, depth - 1, player, false);
+            val = if sval > val { sval } else { val };
         }
+        return val;
     } else {
-        value = 9000;
-        for snode in node.mut_snodes() {
-            let b_value = minimax_r(snode, depth - 1, max_player);
-            value = if b_value > value { value } else { b_value }
+        let mut val = 9000;
+        for snode in node.snodes() {
+            let sval = minimax(snode, depth - 1, player, true);
+            val = if sval > val { val } else { sval };
         }
+        return val;
     }
-
-    value
 }
 
 trait GetNodeScore {
@@ -84,7 +58,7 @@ impl GetNodeScore for Node<Game> {
     fn snodes_boards(&self) -> Vec<&Board> {
         self.snodes_values()
             .iter()
-            .map(|v: &&Game| v.bcontent())
+            .map(|v| v.bcontent())
             .fold(Vec::new(), |mut acc, elem| {
                 acc.push(elem);
                 acc
